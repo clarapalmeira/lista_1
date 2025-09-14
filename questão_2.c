21dc6b692a4ee5efdfa4f5c1b842577d69f0b41c
clarapalmeira <clara.palmeira12@aluno.ifce.edu.br>
2025-09-14 12:27:45 -0300

    Update questão_2.c

--- questão_2.c
+++ questão_2.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main() {
-int x;
+ float x;
     printf("Entre com o valor: ");
-    scanf("%d", &x);
-    printf("Triplo: %d\n", 3 * x);
-    printf("Quadrado: %d\n", x * x);
-    printf("Meio: %.1f\n", x / 2.0);
+    scanf("%f", &x);
+    printf("Valor com uma casa decimal: %.1f\n", x);
     return 0;
 }
