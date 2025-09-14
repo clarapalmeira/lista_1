4be5641d3f02db8b786e5cf050e7002903db5e20
clarapalmeira <clara.palmeira12@aluno.ifce.edu.br>
2025-09-14 12:44:07 -0300

    Create questão_8.c

---
+++ questão_8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+int x;
+    printf("Entre com o valor: ");
+    scanf("%d", &x);
+    printf("Sucessor: %d\n", x + 1);
+     printf("Antecessor: %d\n", x - 1);
+
+    return 0;
+}
