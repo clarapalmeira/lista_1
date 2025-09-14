0158d88b2612b2ae0596ca0b0a99f6e9589198be
clarapalmeira <clara.palmeira12@aluno.ifce.edu.br>
2025-09-14 12:29:37 -0300

    Update questão_5.c

--- questão_5.c
+++ questão_5.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    float x;
+    char y;
+    printf("Entre com a sua altura: ");
+    scanf("%f", &x);
+    printf("Entre com o seu sexo (F/M): ");
+    scanf(" %c", &y);
+
+    if (y == 'M' || y == 'm') {
+        printf("Peso ideal (homens): %.2f\n", 72.7 * x - 58);
+    } else if (y == 'F' || y == 'f') {
+        printf("Peso ideal (mulheres): %.2f\n", 62.1 * x - 44.7);
+    } else {
+        printf("Sexo inválido.\n");
+    }
+
+    return 0;
+}
