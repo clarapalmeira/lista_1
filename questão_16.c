1da9ca283788d7d2dcd25186cb27285461daa2c3
clarapalmeira <clara.palmeira12@aluno.ifce.edu.br>
2025-09-15 08:25:55 -0300

    Create questão_16.c

---
+++ questão_16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+   
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    
+    printf("O modulo (valor absoluto) do numero digitado: %d\n", abs(numero));
+
+    return 0;
+}
